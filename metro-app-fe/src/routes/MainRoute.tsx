4299014605f70be9b9093f4e4bf5a7fb5072a3ff
lcaohoanq <hoangclw@gmail.com>
2025-05-26 23:51:14 +0700

    hotfix: fix to cd

--- metro-app-fe/src/routes/MainRoute.tsx
+++ metro-app-fe/src/routes/MainRoute.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from '../components/Header'
-import Admin from '../pages/admin/Admin'
-import Dashboard from '../pages/admin/components/Dashboard/Dashboard'
-import UserManagement from '../pages/admin/components/User/UserManagement'
-import Station from '../pages/admin/components/Station/Station'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "../components/Header";
+import Admin from "../pages/admin/Admin";
+import Dashboard from "../pages/admin/components/Dashboard/Dashboard";
+import Station from "../pages/admin/components/Station/Station";
+import UserManagement from "../pages/admin/components/User/UserManagement";
 
 export default function MainRoute() {
   return (
     <BrowserRouter>
-    <Header/>
-        <Routes>
-            <Route path='/' element={<Admin/>}>
-              <Route path='' element={<Dashboard/>} />
-              <Route path='users' element={<UserManagement/>} />
-              <Route path='route' element={<Station/>} />
-            </Route>
-        </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Admin />}>
+          <Route path="" element={<Dashboard />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="route" element={<Station />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }

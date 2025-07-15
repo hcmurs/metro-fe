c93b2bf35711261b7c584cccdfa52739aa7f809b
lcaohoanq <hoangclw@gmail.com>
2025-07-16 00:35:23 +0700

    Add ScrollToTop component and update MainRoute imports

--- metro-app-fe/src/App.tsx
+++ metro-app-fe/src/App.tsx
@@ -1,14 +1,16 @@
-import MainRoute from "./routes/MainRoute";
-import './App.css'
-import { AuthProvider } from "./contexts/AuthContext";
 import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import ScrollToTop from "./components/ScrollToTop";
+import { AuthProvider } from "./contexts/AuthContext";
+import MainRoute from "./routes/MainRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
+        <ScrollToTop />
         <MainRoute />
       </AuthProvider>
     </BrowserRouter>
-  )
+  );
 }
